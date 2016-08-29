41f910e0259e0d39afdf37498a81bd9eeb329a9e
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-08-29 08:50:42 -0300

    fix LD_LIBRARY_PATH entry for typelib plugins

--- overrides.rb
+++ overrides.rb
@@ -12,5 +12,5 @@
 # See config.yml to set the prefix:/opt/autoproj/2.0 globally for all packages.
 
 setup_package('typelib') do |pkg|
-    pkg.env_add_path 'LD_LIBRARY_PATH', File.join(pkg.prefix, 'typelib')
+    pkg.env_add_path 'LD_LIBRARY_PATH', File.join(pkg.prefix, 'lib', 'typelib')
 end
